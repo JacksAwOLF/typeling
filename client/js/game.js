1237106acfae9ac7797ad871a37464c02e95d12e
Victor Chen <chiangochen@gmail.com>
2024-01-06 15:09:53 -0800

    fixed scrolling text

--- client/js/game.js
+++ client/js/game.js
@@ -4,12 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 720;
 canvas.id = "canvas";
-// canvas.onwheel = (e) => {
-//   e.preventDefault();
-// };
-// canvas.onmousewheel = (e) => {
-//   e.preventDefault();
-// };
+
 canvas.style =
   "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);";
 document.body.appendChild(canvas);
@@ -17,54 +12,16 @@ document.body.appendChild(canvas);
 // global variables
 let txtReady = false;
 let wordBank = [];
-let lanes = [];
-
-const removeWord = function (i, j) {
-  lanes[i].words.splice(j, 1); // remove word
-  if (lanes[i].lastWordInd > j) lanes[i].lastWordInd -= 1; // update previous word
-};
-
-// create the word input box
-// or should we just draw this in the canvas?
-const wordInput = document.createElement("input");
-wordInput.style = "position: absolute; top: 80%; left: 40%;";
-wordInput.onkeydown = (e) => {
-  if (e.key === " " || e.key === "Enter") {
-    const val = e.target.value.trim();
-    for (let i = 0; i < lanes.length; i++) {
-      for (let j = 0; j < lanes[i].words.length; j++) {
-        if (val === lanes[i].words[j].text) {
-          removeWord(i, j);
-          e.target.value = "";
-          break;
-        }
-      }
-    }
-  }
-
-  if (e.key === " ") e.preventDefault();
-};
-document.body.appendChild(wordInput);
-
-// background image
-let bgReady = false;
-const bgImage = new Image();
-bgImage.onload = function () {
-  bgReady = true;
-};
-bgImage.src = "images/plantsVzombies.jpeg";
-
-// fetch text
-
-// const fs = require("fs");
-// fs.readFile("wordbank/test.txt", (err, data) => {
-//   if (err) throw err;
-//   const text = data.toString();
-//   wordBank = text.split("\n");
-//   wordBank = wordBank.map((line) => line.split(" "));
-//   console.log(wordBank);
-//   txtReady = true;
-// });
+let lanes = [
+  {
+    y: 300,
+    getNewInd: true,
+    lineInd: -1,
+    wordInd: -1,
+    words: [],
+    pps: 100, // pixels per second
+  },
+];
 
 const str = `# rm all files
 git rm -r --cached .
@@ -75,8 +32,80 @@ git commit -m ".gitignore is now working"`;
 const text = str.toString();
 wordBank = text.split("\n");
 wordBank = wordBank.map((line) => line.split(" "));
-console.log(wordBank);
+console.log("wordbank:", wordBank);
 txtReady = true;
+
+// remove the jth word from the ith lane
+const removeWord = function (i, j) {
+  lanes[i].words.splice(j, 1); // remove word
+  if (lanes[i].lastWordInd > j) lanes[i].lastWordInd -= 1; // update previous word
+};
+
+// remove the first matching word from lanes
+// word should also be on screen
+const inputOnKeyDown = (e) => {
+  if (e.key === " " || e.key === "Enter") {
+    const val = e.target.value.trim();
+    for (let i = 0; i < lanes.length; i++) {
+      for (let j = 0; j < lanes[i].words.length; j++) {
+        const word = lanes[i].words[j];
+        if (val === word.text && word.x < canvas.width) {
+          removeWord(i, j);
+          e.target.value = "";
+          break;
+        }
+      }
+    }
+  }
+
+  if (e.key === " ") e.preventDefault();
+};
+
+// add new word to lane i
+const addWordToLane = function (i, ctx) {
+  lanes[i].wordInd += 1;
+
+  // pick a new line
+  if (
+    lanes[i].lineInd === -1 ||
+    lanes[i].wordInd === wordBank[lanes[i].lineInd].length
+  ) {
+    lanes[i].wordInd = 0;
+    lanes[i].lineInd = Math.floor(Math.random() * wordBank.length);
+  }
+
+  // pick next word on the line
+  const lastWord = lanes[i].words[lanes[i].lastWordInd];
+  let x = canvas.width;
+
+  if (lastWord !== undefined) {
+    x = lastWord.x + ctx.measureText(lastWord.text + "  ").width;
+  }
+
+  lanes[i].words.push({
+    x: x,
+    y: lanes[i].y,
+    text: wordBank[lanes[i].lineInd][lanes[i].wordInd],
+  });
+
+  // update last word added
+  lanes[i].lastWordInd = lanes[i].words.length - 1;
+};
+
+// create the word input box
+// or should we just draw this in the canvas?
+const wordInput = document.createElement("input");
+wordInput.style = "position: absolute; top: 80%; left: 40%;";
+wordInput.onkeydown = inputOnKeyDown;
+document.body.appendChild(wordInput);
+
+// background image
+let bgReady = false;
+const bgImage = new Image();
+bgImage.onload = function () {
+  bgReady = true;
+};
+bgImage.src = "images/plantsVzombies.jpeg";
 
 // font and color for main stream of words
 // should i add this to the lane object?
@@ -102,69 +131,26 @@ const init = function () {
   // tower
   tower.xCutoffPercentage = 0.25;
   tower.health = 10;
-
-  // main lane
-  lanes.push({
-    y: 300,
-    getNewInd: true,
-    lineInd: -1,
-    wordInd: -1,
-    words: [],
-    spd: 1,
-  });
 };
 
 const mainLaneInd = 0;
 
-const addWordToLane = function (i) {
-  lanes[i].wordInd += 1;
-
-  // pick a new line if no line picked or all words are draw on the current line
-  if (
-    lanes[i].lineInd === -1 ||
-    lanes[i].wordInd === wordBank[lanes[i].lineInd].length
-  ) {
-    lanes[i].wordInd = 0;
-    lanes[i].lineInd = Math.floor(Math.random() * wordBank.length);
-  }
-
-  // push new word into array
-  lanes[i].words.push({
-    x: canvas.width,
-    y: lanes[i].y,
-    text: wordBank[lanes[i].lineInd][lanes[i].wordInd],
-  });
-
-  // update previous word so we can keep track when to add new word
-  lanes[i].lastWordInd = lanes[i].words.length - 1;
-};
-
 // update variables
 const update = function (delta) {
   if (txtReady) {
+    // add words if necessary
     ctx.font = mainTextFont;
     ctx.fillStyle = mainTextColor;
     for (let i = 0; i < lanes.length; i++) {
-      if (lanes[i].wordInd === -1) {
-        addWordToLane(i);
-        continue;
-      }
-
-      const prevText = wordBank[lanes[i].lineInd][lanes[i].wordInd];
-      const wordLen = ctx.measureText(prevText + "  ").width;
-      const prevWord = lanes[i].words[lanes[i].lastWordInd];
-
-      // draw a new word if previous word scrolled past the screen
-      if (prevWord !== undefined && prevWord.x + wordLen < canvas.width) {
-        addWordToLane(i);
+      while (lanes[i].words.length < 50) {
+        addWordToLane(i, ctx);
       }
     }
 
     // update each word x position in each lane
     for (let i = 0; i < lanes.length; i++) {
       for (let j = 0; j < lanes[i].words.length; j++) {
-        lanes[i].words[j].x -= lanes[i].spd;
-
+        lanes[i].words[j].x -= lanes[i].pps * delta;
         // check if hit the tower
         if (lanes[i].words[j].x < canvas.width * tower.xCutoffPercentage) {
           tower.health -= 1;
@@ -214,9 +200,11 @@ const render = function () {
   // ctx.fillText("Hello", 10, 50);
 };
 
+let then = Date.now();
 const main = function () {
   let now = Date.now();
   let delta = now - then;
+  then = now;
   update(delta / 1000);
   render();
 
@@ -231,8 +219,6 @@ requestAnimationFrame =
   w.webkitRequestAnimationFrame ||
   w.msRequestAnimationFrame ||
   w.mozRequestAnimationFrame;
-
-let then = Date.now();
 
 init();
 main();
