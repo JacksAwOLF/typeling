925ff083b47672c49bb9e03dd08859b72613bc5b
Victor Chen <chiangochen@gmail.com>
2024-01-06 20:00:41 -0800

    change window size

--- server/js/gvars.js
+++ server/js/gvars.js
@@ -1,5 +1,5 @@
-export const canvW = 480;
-export const canvH = 360;
+export const canvW = 1024;
+export const canvH = 720;
 
 // percentage of screen where words deal damage
 export const xCutoffPercentage = 0.25;
